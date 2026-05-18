4fc963a97a32a7afc8755c558f1eea3d3aa72a7d
ARONER24 <77134350@mail.isil.pe>
2026-05-17 23:25:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.write("Mueve el control deslizante para calcular la nota estimada en el exame
 
 # --- REQUISITO OBLIGATORIO: Datos del estudiante y Colab ---
 st.sidebar.header("Información del Estudiante")
-st.sidebar.write("**Nombre:** Aron Limache")  # Tu preferencia de nombre
-st.sidebar.write("**Código ISIL:** [Ingresa aquí tu código]") 
+st.sidebar.write("**Nombre:Luis Aron Limache Baldera")  # Tu preferencia de nombre
+st.sidebar.write("**Código ISIL:** [77134350]") 
 
 # IMPORTANTE: Reemplaza este enlace por el link de compartir de tu Colab (modo Lector)
 url_colab = "https://colab.research.google.com/drive/tu_enlace_aqui"
